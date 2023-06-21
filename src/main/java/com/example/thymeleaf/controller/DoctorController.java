472373834bfcc9d19bd863ff2090ec00c49e0458
Naailah JUGON <jugonnaailah@gmail.com>
2023-06-21 16:35:00 +0200

    finalized challenge thymleaf

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,6 +1,8 @@
 package com.example.thymeleaf.controller;
 
+import com.example.thymeleaf.model.Doctor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DoctorController {
 
     @GetMapping("/doctor/")
-    public String doctor(@RequestParam(required=false, defaultValue="0") int number,
+    public String doctor(Doctor doctor, Model model, @RequestParam(required=false, defaultValue="0") int number,
                          @RequestParam(required=false, defaultValue="John Smith") String name) {
-
+        Doctor doc = new Doctor(number, name);
+        model.addAttribute("naai", doc);
+//        model.addAttribute("name", name);
+//        model.addAttribute("number", number);
         return "doctor";
     }
 }
